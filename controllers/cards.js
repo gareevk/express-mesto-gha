4dc7a3a8517d4ec02075ed9e1f0576f7e7f41e6d
Konstantin Gareev <gareevk@hotmail.com>
2022-05-06 03:02:09 +0300

    error handling #4

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,20 +6,38 @@ module.exports.getCards = (req, res) => {
   .catch( err => res.status(500).send( { message: err.message } ));
 }
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = async (req, res) => {
+  try {
+    if (req.params.cardId.length !== 24) {
+      res.status(400).send({ message: 'Передан некорректный id карточки' });
+      return;
+    }
+    const deleteCard = await Card.findById(req.params.cardId);
+    console.log(deleteCard);
+    if (!deleteCard) {
+      res.status(404).send({ message: 'Карточка не найдена' });
+      return;
+    }
+    await Card.findByIdAndRemove(req.params.cardId);
+    res.status(200).send( {data: deleteCard} );
+  } catch(err) {
+    res.status(500).send( {message: err.message} );
+  }
+  /*
   if (req.params.cardId !== 24) {
     res.status(400).send({ message: 'Передан некорректный id карточки' });
     return;
   }
   Card.findByIdAndRemove(req.params.cardId)
   .then( card => {
-    if (card === null) {
+    if (!card) {
       res.status(404).send({ message: 'Карточка не найдена' });
       return;
     }
     res.status(200).send( {data: card} )
   })
   .catch( err => res.status(500).send( {message: err.message} ));
+  */
 }
 
 module.exports.createCard = async (req, res) => {
@@ -37,7 +55,6 @@ module.exports.createCard = async (req, res) => {
   }
 }
 
-/*
 module.exports.likeCard = async (req, res) => {
   try {
     if (req.params.cardId.length !== 24) {
@@ -49,37 +66,10 @@ module.exports.likeCard = async (req, res) => {
       { $addToSet: { likes: req.user._id } },
       { new: true },
     );
-    if (card === null) {
+    if (!card) {
       res.status(404).send({ message: 'Карточка не найдена' });
       return;
     };
-    res.status(200).send( {data: card});
-  } catch(err) {
-    if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Передан некорректный id карточки', err });
-      return;
-    }
-    res.status(500).send( {message: err.message} );
-  }
-}
-*/
-
-module.exports.likeCard = async (req, res) => {
-  try {
-    const card = await Card.findById(req.params.cardId);
-     if (card === null) {
-      res.status(404).send({ message: 'Карточка не найдена' });
-      return;
-    } else if (req.params.cardId.length !== 24) {
-      res.status(400).send({ message: 'Передан некорректный id карточки' });
-      return;
-    };
-    await Card.findByIdAndUpdate(
-      req.params.cardId,
-      { $addToSet: { likes: req.user._id } },
-      { new: true },
-    );
-
     res.status(200).send( {data: card});
   } catch(err) {
     if (err.name === 'CastError') {
@@ -102,7 +92,7 @@ module.exports.dislikeCard = async (req, res) => {
       { $pull: { likes: req.user._id } },
       { new: true },
     );
-    if (card === null) {
+    if (!card) {
       res.status(404).send({ message: 'Карточка не найдена' });
       return;
     }
