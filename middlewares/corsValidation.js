b43e342289484ef926678e42ddc0967f3a4ff9e0
Konstantin Gareev <gareevk@hotmail.com>
2022-05-28 01:03:52 +0300

    eslint fixes

--- middlewares/corsValidation.js
+++ middlewares/corsValidation.js
@@ -2,10 +2,10 @@
 const allowedCors = require('../utils/allowedCors');
 
 module.exports.corsValidation = (req, res, next) => {
-    const { origin } = req.headers;
-    if (allowedCors.includes(origin)) {
-        res.header('Access-Control-Allow-Origin', origin);
-    };
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
 
-    next();
-}
+  next();
+};
